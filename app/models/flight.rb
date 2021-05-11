c775d321f88777bfa0cf6c4dfb39a0b7373997c8
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-12 09:58:24 +1000

    added Reservations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,3 @@
 class Flight < ApplicationRecord
+  belongs_to :plane, :optional => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_231203) do
+ActiveRecord::Schema.define(version: 2021_05_11_235541) do
 
   create_table "flights", force: :cascade do |t|
     t.integer "number"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2021_05_11_231203) do
     t.string "name"
     t.integer "rows"
     t.integer "colums"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "seat_id"
+    t.integer "user_id"
+    t.integer "flight_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
