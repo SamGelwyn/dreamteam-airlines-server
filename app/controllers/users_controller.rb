b47b4ab264fc1a848d4a940eeb76b040c8216085
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-12 14:31:09 +1000

    plane assoc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :admin)
+      params.require(:user).permit(:name, :email, :password, :pasword_confirmation)
     end
 end
