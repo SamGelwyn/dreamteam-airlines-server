61e3c82f33db7b1f48a25aca7189b751c994e975
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-13 12:31:53 +1000

    model validation

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,9 +4,9 @@ belongs_to :plane
 has_many :reservations
 # has_many :users, through: :reservations
 
-# validates :number, :presence => true
-# validates :origin, :presence => true
-# validates :destination, :presence => true
-# validates :date, :presence => true
+validates :number, :presence => true
+validates :origin, :presence => true
+validates :destination, :presence => true
+validates :date, :presence => true
 
 end
